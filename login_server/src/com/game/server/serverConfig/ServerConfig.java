155ed22330ea21c284da57206846a8c4a18902c2
mifeng <646330494@qq.com>
2020-07-14 16:27:58 +0800

    config commit

--- login_server/src/com/game/server/serverConfig/ServerConfig.java
+++ login_server/src/com/game/server/serverConfig/ServerConfig.java
@@ -11,5 +11,5 @@ public class ServerConfig {
     public static final String HTTP_SERVER_IP = "192.168.1.217";
     public static final int HTTP_PORT = 8000;
     public static final String REGION_LOGIN = "server";
-    public static final String GATE_SERVER_IP = "tcp://192.168.1.217:5000";
+    public static final String GATE_SERVER_IP = "tcp://127.0.0.1:5000";
 }
